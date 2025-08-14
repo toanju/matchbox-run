3702a4027e855aae49303b2323c1b08c03faa380
Tobias Jungel <1773291+toanju@users.noreply.github.com>
2025-08-14 09:38:21 +0200

    Improve setup script
    * bump matchbox version
    * setup error trap
    * only download or create files when necessary
    * ease configuration
    * support podman or docker depending on availability

--- setup.sh
+++ setup.sh
@@ -1,79 +1,85 @@
 #!/bin/bash
 
-set -euo pipefail
+set -Eeuo pipefail
 
-MATCHBOX_IMAGE="quay.io/poseidon/matchbox:v0.10.0"
+# setup error trap
+trap 'echo "An error occurred in line ${LINENO}. Exiting..."; exit 1' ERR
+
+MATCHBOX_IMAGE="quay.io/poseidon/matchbox:v0.11.0"
+MATCHBOX_IP="10.10.10.1"
+MATCHBOX_DOMAIN="matchbox.lan"
+
 CERTS_DIR="certs"
 
 FLATCAR_CHANNEL=beta
 
+UEFI_FW_REPO="toanju/RPi4"
 UEFI_FW_DIR=lib/assets/uefi
 UEFI_FW_VERSIONFILE=latest-version
-UEFI_FW_VERSION=$(curl --silent "https://api.github.com/repos/pftf/RPi4/releases/latest" | jq -r .tag_name)
 
 ARCH=arm64
 
-function download_uefi_firmware() {
-  pushd ${UEFI_FW_DIR} || exit 1
-  curl -sLO "https://github.com/pftf/RPi4/releases/download/${UEFI_FW_VERSION}/RPi4_UEFI_Firmware_${UEFI_FW_VERSION}.zip"
-  popd || exit 2
-}
+CONTAINER_RUNTIME="$(command -v podman || command -v docker)"
+if [[ -z "${CONTAINER_RUNTIME}" ]]; then
+  echo "No container runtime found. Please install Podman or Docker."
+  exit 1
+fi
 
-function write_firmware_versionfile() {
-  pushd $UEFI_FW_DIR || exit 1
+function download_uefi_firmware() {
+  UEFI_FW_VERSION=$(curl --silent "https://api.github.com/repos/${UEFI_FW_REPO}/releases/latest" | jq -r .tag_name)
+  # check if FW version file exists and matches the latest version
+  if [[ -f ${UEFI_FW_DIR}/${UEFI_FW_VERSIONFILE} ]]; then
+    CURRENT_VERSION=$(cat ${UEFI_FW_DIR}/${UEFI_FW_VERSIONFILE})
+    if [[ "${CURRENT_VERSION}" == "${UEFI_FW_VERSION}" ]]; then
+      echo "UEFI firmware is already up to date: ${CURRENT_VERSION}"
+      return
+    else
+      echo "Updating UEFI firmware from ${CURRENT_VERSION} to ${UEFI_FW_VERSION}"
+    fi
+  fi
+  mkdir -p ${UEFI_FW_DIR}
+  pushd ${UEFI_FW_DIR} || exit 1
+    curl -sLO "https://github.com/${UEFI_FW_REPO}/releases/download/${UEFI_FW_VERSION}/RPi4_UEFI_Firmware_${UEFI_FW_VERSION}.zip"
     echo "$UEFI_FW_VERSION" > $UEFI_FW_VERSIONFILE
   popd || exit 2
 }
 
 function create_tls_certs() {
+  if [[ -d ${CERTS_DIR} ]]; then
+    echo "Directory ${CERTS_DIR} already exists, skipping TLS cert generation."
+    return
+  fi
+  mkdir -p ${CERTS_DIR}
   pushd ${CERTS_DIR} || exit 1
     curl -sOL https://raw.githubusercontent.com/poseidon/matchbox/main/scripts/tls/cert-gen
     curl -sOL https://raw.githubusercontent.com/poseidon/matchbox/main/scripts/tls/openssl.conf
     chmod 755 cert-gen
 
-    export SAN=DNS.1:matchbox.lan,IP.1:10.10.10.1
+    export SAN=DNS.1:${MATCHBOX_DOMAIN},IP.1:${MATCHBOX_IP}
     ./cert-gen
-
   popd || exit 2
 }
 
 function download_flatcar() {
-  ./get-flatcar "${FLATCAR_CHANNEL}" current ./lib/assets "$ARCH"
-  eval "$(grep FLATCAR_VERSION= lib/assets/flatcar/current/version.txt)"
-  rsync -av ./lib/assets/flatcar/current/* "./lib/assets/flatcar/$FLATCAR_VERSION/"
+  # check current release version
+  curl -LO "https://${FLATCAR_CHANNEL}.release.flatcar-linux.net/${ARCH}-usr/current/version.txt"
+  eval "$(grep FLATCAR_VERSION= version.txt)"
+  rm version.txt
+  if [[ -z "${FLATCAR_VERSION}" ]]; then
+    echo "Failed to determine Flatcar version from version.txt."
+    exit 1
+  fi
+  if [[ -d "./lib/assets/flatcar/${FLATCAR_VERSION}" ]]; then
+    echo "Flatcar version ${FLATCAR_VERSION} already exists, skipping download."
+    return
+  fi
+ 
+  ./get-flatcar "${FLATCAR_CHANNEL}" "$FLATCAR_VERSION" ./lib/assets "$ARCH"
 }
 
-function download_k8s_tools() {
-  VERSION=$1
-  DL_DIR="./lib/assets/k8s/$VERSION/$ARCH"
-  mkdir -p "$DL_DIR"
-  for bin in kubectl kubeadm kubelet; do
-    curl -L "https://dl.k8s.io/release/${VERSION}/bin/linux/${ARCH}/${bin}" -o "$DL_DIR/${bin}"
-  done
-}
+# start here
+create_tls_certs
+download_uefi_firmware
+download_flatcar
 
-function download_k8s_release_artifacts() {
-  VERSION=$1
-  DL_DIR="./lib/assets/k8s/$VERSION"
-  mkdir -p "$DL_DIR"
-  DOWNLOAD_DIR="/opt/bin"
-  curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${VERSION}/cmd/kubepkg/templates/latest/deb/kubelet/lib/systemd/system/kubelet.service" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" > "${DL_DIR}/kubelet.service"
-  curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${VERSION}/cmd/kubepkg/templates/latest/deb/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" > "${DL_DIR}/10-kubeadm.conf"
-}
-
-function get_latest_github_release_tag() {
-  repo=$1
-  curl --silent "https://api.github.com/$repo/release/releases/latest" | jq -r '.tag_name'
-  return $?
-}
-
-# XXX start here
-mkdir -p ${UEFI_FW_DIR} ${CERTS_DIR}
-# create_tls_certs
-# download_uefi_firmware
-# write_firmware_versionfile
-#download_flatcar
-#download_k8s_tools "$(curl -L -s https://dl.k8s.io/release/stable.txt)"
-#download_k8s_release_artifacts "$(get_latest_github_release_tag repos/kubernetes)"
-
-podman run --net=host --rm -v ./lib:/var/lib/matchbox:Z -v ./$CERTS_DIR:/etc/matchbox:Z,ro "$MATCHBOX_IMAGE" -address=0.0.0.0:8080 -rpc-address=0.0.0.0:8081 -log-level=debug
+$CONTAINER_RUNTIME run --net=host --rm -v ./lib:/var/lib/matchbox:Z -v ./$CERTS_DIR:/etc/matchbox:Z,ro "$MATCHBOX_IMAGE" -address=0.0.0.0:8080 -rpc-address=0.0.0.0:8081 -log-level=debug
